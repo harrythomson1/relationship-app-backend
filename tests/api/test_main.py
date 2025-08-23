1075f56d42a3c054c24752b584a35d224487d828
Harry Thomson <haroldt95@hotmail.com>
2025-08-23 15:46:22 +0700

    Add a test for confirming the healthcheck responds correctly

---
+++ tests/api/test_main.py
@@ -0,0 +1,13 @@
+import pytest
+from httpx import ASGITransport, AsyncClient
+
+from api.main import app
+
+@pytest.mark.asyncio
+async def test_health():
+    async with AsyncClient(
+        transport=ASGITransport(app=app), base_url="http://test"
+    ) as ac:
+        response = await ac.get("/health")
+        assert response.json() == {"status": "ok"}
+
