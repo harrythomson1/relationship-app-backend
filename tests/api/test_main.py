a1d80022d76c77eb1af55466c8e646e99f9fb161
Harry Thomson <haroldt95@hotmail.com>
2025-08-23 15:48:28 +0700

    Confirm that the health check responds with a 200

--- tests/api/test_main.py
+++ tests/api/test_main.py
@@ -9,5 +9,6 @@ async def test_health():
         transport=ASGITransport(app=app), base_url="http://test"
     ) as ac:
         response = await ac.get("/health")
+        assert response.status_code == 200
         assert response.json() == {"status": "ok"}
 
