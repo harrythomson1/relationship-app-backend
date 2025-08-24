18359bfc127e56ee21c0eed864882eb406072211
Harry Thomson <haroldt95@hotmail.com>
2025-08-24 14:43:47 +0700

    Set a raise condition if the DATABASE_URL is not set

--- migrations/env_settings.py
+++ migrations/env_settings.py
@@ -1,5 +1,8 @@
 import os
 from dotenv import load_dotenv
 load_dotenv()
-def db_url():
-    return os.getenv("DATABASE_URL")
+def db_url() -> str:
+    url = os.getenv("DATABASE_URL")
+    if url is None:
+        raise RuntimeError("DATABASE_URL is not set")
+    return url
