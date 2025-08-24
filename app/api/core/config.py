92e5fcec93594118c39fdab3e30417649afa8b0f
Harry Thomson <haroldt95@hotmail.com>
2025-08-24 17:27:00 +0700

    Update config to error if the DATABASE_URL is None

--- app/api/core/config.py
+++ app/api/core/config.py
@@ -1,7 +1,12 @@
 import os
+from typing import Final
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+_url = os.getenv("DATABASE_URL")
+if _url is None:
+    raise RuntimeError("DATABASE_URL is not set")
+
+DATABASE_URL: Final[str] = _url
