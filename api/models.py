878b64aa1adbb04d1514913ed53e32ea7cec9733
Harry Thomson <haroldt95@hotmail.com>
2025-08-24 15:26:42 +0700

    Remove examples table and include users table

--- api/models.py
+++ api/models.py
@@ -1,10 +1,14 @@
-from sqlalchemy import String
+from datetime import datetime
+
+from sqlalchemy import DateTime, String, func
 from sqlalchemy.orm import Mapped, mapped_column
 
 from .db import Base
 
 
-class Example(Base):
-    __tablename__ = "examples"
+class User(Base):
+    __tablename__ = "users"
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    email: Mapped[str] = mapped_column(String(120), unique=True)
     name: Mapped[str] = mapped_column(String(120))
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
