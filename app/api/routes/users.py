2cde0889b41bb64cf717c13e3403bd2aad53d6fa
Harry Thomson <haroldt95@hotmail.com>
2025-08-24 17:02:35 +0700

    Create post route
    Currently there's no user service or get user service these will be added from the top down

--- app/api/routes/users.py
+++ app/api/routes/users.py
@@ -1,8 +1,22 @@
-from fastapi import APIRouter
+import logging
+
+from fastapi import APIRouter, Depends, status
+
+from app.api.schemas.user_schema import UserCreate, UserSchema
 
 router = APIRouter()
+logger = logging.getLogger(__name__)
 
 
 @router.get("/users")
 def test_route():
     return {"Are users working?": "Yes"}
+
+
+@router.post("/users", status_code=status.HTTP_201_CREATED, response_model=UserSchema)
+async def add_user(user_info: UserCreate, service: UsersService = Depends(get_users_service)):
+    try:
+        user = await service.add_user(user_info)
+        return user
+    except Exception:
+        logger.exception("Unexpected error with adding a user")
