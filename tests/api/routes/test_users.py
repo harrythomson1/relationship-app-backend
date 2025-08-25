eb1cf118d9c8ae8f38f992beecdb09af9b018695
Harry Thomson <haroldt95@hotmail.com>
2025-08-25 17:12:51 +0700

    Test the new patch route

--- tests/api/routes/test_users.py
+++ tests/api/routes/test_users.py
@@ -38,3 +38,17 @@ async def test_get_user_successfully(client):
     assert body["name"] == "Harry"
     assert "id" in body and body["id"] > 0
     assert "created_at" in body
+
+
+@pytest.mark.asyncio
+async def test_update_user_name_successfully(client):
+    payload = {"email": "harry@example.com", "name": "Harry"}
+    res = await client.post("/users", json=payload)
+    payload = {"name": "New Harry"}
+    assert res.status_code == 201
+    created_user = res.json()
+    user_id = created_user["id"]
+    res = await client.patch(f"/users/{user_id}", json=payload)
+    assert res.status_code == 200
+    body = res.json()
+    assert body["name"] == "New Harry"
