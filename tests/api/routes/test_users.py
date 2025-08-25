dc220c15550c50c9f5862239030b164ce1c46707
Harry Thomson <haroldt95@hotmail.com>
2025-08-25 15:16:13 +0700

    Change test so it no longer needs to be unique now the test DB flushes

--- tests/api/routes/test_users.py
+++ tests/api/routes/test_users.py
@@ -1,11 +1,9 @@
-from uuid import uuid4
-
 import pytest
 
 
 @pytest.mark.asyncio
 async def test_create_user(client):
-    email = f"harry+{uuid4().hex[:8]}@example.com"
+    email = "harry@example.com"
     payload = {"email": email, "name": "Harry"}
 
     res = await client.post("/users", json=payload)
