62bb436d847cba97851e0b5bb35a6147f5c11295
Harry Thomson <haroldt95@hotmail.com>
2025-08-27 15:42:27 +0700

    Rename UserService file

---
+++ app/api/services/users_service.py
@@ -0,0 +1,20 @@
+from app.api.models import User
+
+
+class UsersService:
+    def __init__(self, repository):
+        self.repository = repository
+
+    async def add(self, user_info):
+        user = User(name=user_info.name, email=user_info.email)
+        return await self.repository.add(user)
+
+    async def get_by_id(self, id):
+        user = await self.repository.get_by_id(id)
+        return user
+
+    async def update(self, id, update_data):
+        return await self.repository.update(id, update_data)
+
+    async def delete(self, id):
+        return await self.repository.delete(id)
