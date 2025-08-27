9c168fda60dae85fe813e08241ea6815aa6d5fb3
Harry Thomson <haroldt95@hotmail.com>
2025-08-27 14:32:40 +0700

    Verify JWT's

--- app/api/core/security.py
+++ app/api/core/security.py
@@ -2,7 +2,8 @@ import os
 from datetime import datetime, timedelta
 
 from dotenv import load_dotenv
-from jose import jwt
+from fastapi import HTTPException, status
+from jose import JWTError, jwt
 
 load_dotenv()
 
@@ -21,3 +22,18 @@ def create_access_token(payload: dict, expires_minutes: int = 30):
     to_encode.update({"exp": expire, "iat": now, "sub": str(payload.get("sub"))})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+
+def verify_access_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id: str | None = payload.get("sub")
+        if user_id is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED, detail="Token is missing subject claim"
+            )
+        return payload
+    except JWTError as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Token is invalid or expired"
+        ) from e
