3014f47d021335a0f78fae0a93ef6c80a170c54e
Harry Thomson <haroldt95@hotmail.com>
2025-08-27 14:49:24 +0700

    Add dev_login route

--- app/api/main.py
+++ app/api/main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.routes import users
+from app.api.routes.auth import dev_login
 
 app = FastAPI(title="Relationship App API", version="0.1.0")
 
@@ -15,6 +16,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 app.include_router(users.router)
+app.include_router(dev_login.router)
 
 
 @app.get("/health")
