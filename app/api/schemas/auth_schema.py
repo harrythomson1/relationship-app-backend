de6f3f7d09394e850aa34950f0b53ac76adf1a4e
Harry Thomson <haroldt95@hotmail.com>
2025-08-27 14:01:39 +0700

    Create schemas for logging in

---
+++ app/api/schemas/auth_schema.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel, EmailStr
+
+from app.api.schemas.user_schema import UserSchema
+
+
+class DevLoginRequest(BaseModel):
+    email: EmailStr
+    name: str
+
+
+class AuthResponse(BaseModel):
+    user: UserSchema
+    token: str
