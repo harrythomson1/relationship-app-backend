22364df36f5b7f49883427f2c53082ffafa0c61e
Harry Thomson <haroldt95@hotmail.com>
2025-09-09 14:33:27 +0700

    Set up relationship service dependency

--- app/api/dependencies.py
+++ app/api/dependencies.py
@@ -2,7 +2,9 @@ from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.api.core.database_connection import get_db
+from app.api.repositories.relationship_repository import RelationshipRepository
 from app.api.repositories.user_repository import UserRepository
+from app.api.services.relationships_service import RelationshipsService
 from app.api.services.users_service import UsersService
 
 db_dependency = Depends(get_db)
@@ -12,8 +14,17 @@ async def get_user_repository(db: AsyncSession = db_dependency):
     return UserRepository(db)
 
 
+async def get_relationship_repository(db: AsyncSession = db_dependency):
+    return RelationshipRepository
+
+
 user_repository_dependency = Depends(get_user_repository)
+relationship_repository_dependency = Depends(get_relationship_repository)
 
 
 async def get_users_service(user_repostitory=user_repository_dependency):
     return UsersService(repository=user_repostitory)
+
+
+async def get_relationships_service(relationship_repository=relationship_repository_dependency):
+    return RelationshipsService(repository=relationship_repository)
