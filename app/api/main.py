da58007d0f1f37c43a59a47a8eeb085b6e46a42f
Harry Thomson <haroldt95@hotmail.com>
2025-09-18 17:16:02 +0700

    Remove dev_login

--- app/api/main.py
+++ app/api/main.py
@@ -4,7 +4,6 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.routes import invites, me, relationships, users
-from app.api.routes.auth import dev_login
 
 app = FastAPI(title="Relationship App API", version="0.1.0")
 
@@ -17,7 +16,6 @@ app.add_middleware(
 )
 app.include_router(me.router)
 app.include_router(users.router)
-app.include_router(dev_login.router)
 app.include_router(relationships.router)
 app.include_router(invites.router)
 
