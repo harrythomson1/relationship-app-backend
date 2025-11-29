7e8b0f44566e6092241317b3362520ce3c11546d
Harry Thomson <haroldt95@hotmail.com>
2025-11-29 16:42:57 +0545

    Change repo to be more specific as I'm going to be adding the user repo

--- app/api/services/relationships_service.py
+++ app/api/services/relationships_service.py
@@ -6,8 +6,8 @@ class InvalidInviteUserError(Exception):
 
 
 class RelationshipsService:
-    def __init__(self, repository, db):
-        self.repository = repository
+    def __init__(self, relationship_repository, db):
+        self.relationship_repository = relationship_repository
         self.db = db
 
     async def add(self, current_user, relationship_info, user_service, relationship_invite_service):
@@ -19,24 +19,27 @@ class RelationshipsService:
             if current_user.id == partner.id:
                 raise ValueError("A relationship must have two distinct users")
             rel = Relationship(type=relationship_info.type, status=relationship_info.status)
-            rel = await self.repository.add_relationship(rel)
+            rel = await self.relationship_repository.add_relationship(rel)
             rel_member_1 = RelationshipMember(
                 relationship_id=rel.id, user_id=current_user.id, role=relationship_info.role
             )
             rel_member_2 = RelationshipMember(
                 relationship_id=rel.id, user_id=partner.id, role=relationship_info.role
             )
-            await self.repository.add_relationship_members(rel_member_1)
-            await self.repository.add_relationship_members(rel_member_2)
+            await self.relationship_repository.add_relationship_members(rel_member_1)
+            await self.relationship_repository.add_relationship_members(rel_member_2)
         await self.db.commit()
         return rel
 
+    # async def get_partner_time_zone(self, current_user):
+    #     partner = await self.relationship_repository.get_partner(current_user)
+
     async def get_by_id(self, id, current_user):
-        rel = await self.repository.get_by_id(id, current_user)
+        rel = await self.relationship_repository.get_by_id(id, current_user)
         return rel
 
     async def update(self, id, update_data):
-        return await self.repository.update(id, update_data)
+        return await self.relationship_repository.update(id, update_data)
 
     async def delete(self, user_id):
-        return await self.repository.delete(user_id)
+        return await self.relationship_repository.delete(user_id)
