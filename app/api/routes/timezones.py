6ff9a033261f91b0cc717670b2fb629c7778a0c1
Harry Thomson <haroldt95@hotmail.com>
2025-11-29 17:13:47 +0545

    Create timezone endpoint with associated test

--- app/api/routes/timezones.py
+++ app/api/routes/timezones.py
@@ -1,13 +1,14 @@
 from fastapi import APIRouter, Depends
 
 from app.api.auth.utils import get_current_user
-from app.api.dependencies import get_relationships_service
+from app.api.dependencies import get_relationships_service, get_user_repository
 from app.api.schemas.timezone_schema import TimeZoneSchema
 from app.api.services.relationships_service import RelationshipsService
 
 router = APIRouter(prefix="/relationships", tags=["relationships"])
 get_current_user_dependency = Depends(get_current_user)
 relationship_service_dependency = Depends(get_relationships_service)
+user_repository_dependency = Depends(get_user_repository)
 
 
 @router.get("/timezones", response_model=TimeZoneSchema)
@@ -15,5 +16,5 @@ async def get_partner_timezone(
     service: RelationshipsService = relationship_service_dependency,
     current_user=get_current_user_dependency,
 ):
-    breakpoint()
-    time_zone = await service.get_partner_time_zone(current_user)
+    partner_details = await service.get_partner_time_zone(current_user)
+    return partner_details
--- tests/api/routes/test_timezones.py
+++ tests/api/routes/test_timezones.py
@@ -11,11 +11,11 @@ class TestTimezoneGet:
     async def test_get_partner_timezone_success(self, client: AsyncClient):
         res = await _create_authed_relationship(client)
         assert res.status_code == 201, res.text
-        rel_id = res.json()["id"]
 
-        get_res = await client.get("/relationships/timezones")
-        assert get_res.status_code == 200, get_res.text
-        body = get_res.json()
-        assert body["id"] == rel_id
-        assert body["type"] == "romantic"
-        assert body["status"] == "active"
+        res = await client.get("/relationships/timezones")
+
+        assert res.status_code == 200, res.text
+        body = res.json()
+
+        assert body["time_zone"] == "Europe/London"
+        assert body["name"] == "Test Name"
