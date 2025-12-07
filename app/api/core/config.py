e3013911d6a25419de3f4d141959fca988e52402
Harry Thomson <haroldt95@hotmail.com>
2025-12-07 09:17:36 +0545

    Load database URL from local if it exists

--- app/api/core/config.py
+++ app/api/core/config.py
@@ -1,9 +1,13 @@
 import os
+from pathlib import Path
 from typing import Final
 
 from dotenv import load_dotenv
 
-load_dotenv()
+if Path(".env.local").exists():
+    load_dotenv(".env.local")
+else:
+    load_dotenv()
 
 _url = os.getenv("DATABASE_URL")
 if _url is None:
